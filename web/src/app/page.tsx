a2c6d0b416664754158c69352cf6df3fe88ac62b
fearanha <felipearanha.c@gmail.com>
2023-05-16 15:14:05 -0300

    before tailWindCSS

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/Button";
+
 export default function Home() {
   return (
-    <h1>go codee</h1>
-  )
+    <>
+      <h1>go codee</h1>
+      <Button title="Felipe"/>
+      <Button title="Felipe II"/>
+      <Button title="Felipe III"/>
+    </>
+  );
 }
---
+++ web/src/components/Button.tsx
@@ -0,0 +1,8 @@
+interface ButtonProps {
+    title: string;
+}
+export function Button(props: ButtonProps) {
+    return(
+        <p>{props.title}</p>
+    )
+}
