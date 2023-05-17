02ddc737dbc3b1eadaf83eb720e946969e2b8ba5
fearanha <felipearanha.c@gmail.com>
2023-05-17 13:15:54 -0300

    blur background | Stripes

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -50,8 +50,16 @@ module.exports = {
         },
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        stripes:
+          'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
+      },
+
+      backgroundSize: {
+        stripes: '100% 8px',
+      },
+      
+      blur: {
+        full: '194px'
       },
     },
   },
