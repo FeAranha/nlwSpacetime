8dc018e8255ca824caf9a4bcd0838cad5d7c346c
fearanha <felipearanha.c@gmail.com>
2023-05-17 13:24:57 -0300

    bg stars

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return ( 
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
          {/* Blur */}
          <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 rounded-full blur-full"/>
 
@@ -11,9 +11,9 @@ export default function Home() {
       </div>
 
       {/* Right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360ppx] text-center leading-relaxed">
+          <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{' '}
             {/* TODO: link page create */}
             <a href="" className="underline hover:text-gray-50">
