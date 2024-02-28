ed3ddc0d1aa1faa16a8940602c751dade4f429fe
FeAranha <felipearanha.c@gmail.com>
2024-02-27 21:27:08 -0300

    layout da listagem: estatico

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, TouchableOpacity, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
 import * as SecureStore from 'expo-secure-store'
 
@@ -36,6 +36,39 @@ export default function Memory() {
               <Icon name="plus" size={16} color="#000" />
             </TouchableOpacity>
           </Link>
+        </View>
+      </View>
+      <View className="mt-6 space-y-10">
+        <View className="space-y-4">
+          <View className="flex-row items-center gap-2">
+            <View className="h-px w-5 bg-gray-50" />
+            <Text className="font-body text-sm text-gray-100">
+              27 de fevereiro, 2024
+            </Text>
+          </View>
+          <View className="space-y-4 px-8">
+            <Image
+              alt=""
+              className="aspect-video w-full rounded-lg"
+              source={{
+                uri: 'http://localhost:3333/uploads/e9fdfc9c-deb6-4585-ab33-6fdbeeb2d7dd.jpg',
+              }}
+            />
+            <Text className="font-body text-base leading-relaxed text-gray-100">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
+              deserunt eum magnam consectetur, quis nam ut fuga velit corrupti
+              ipsum! Odit reiciendis, illo voluptatum consequuntur earum quo
+              praesentium aperiam eligendi.
+            </Text>
+            <Link href="/memories/id">
+              <TouchableOpacity className="flex-row items-center gap-2">
+                <Text className="font-body text-sm text-gray-200">
+                  Ler mais
+                </Text>
+                <Icon name="arrow-right" size={16} color="#9e9ea0" />
+              </TouchableOpacity>
+            </Link>
+          </View>
         </View>
       </View>
     </ScrollView>
